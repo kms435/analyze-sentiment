f9626e4cd93dce194065134fa537f126cea48b7c
kms435 <kms435@cornell.edu>
2017-11-30 15:18:16 -0500

    remove downloads

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 # ./app.py
 from flask import Flask
 from flask import request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 
 @app.route('/analyze')
 def index():
 	text = request.args.get('text')
-	return text
+	analyzer = SentimentIntensityAnalyzer()
+	vs = analyzer.polarity_scores(text)
+	return vs
 
 if __name__ == "__main__":
 	app.run()
